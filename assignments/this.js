c8fe2839fbe7c5bae5a6696c73cfc6b0f1d498b1
BrandonHarrison01 <49922064+BrandonHarrison01@users.noreply.github.com>
2019-05-08 15:00:26 -0600

    WIP: finished this page

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window/Global Object Binding: this is referring to the global scope.
+* 2. Implicit Binding: this is referring to whatever is left of the dot when calling a function
+* 3. New Binding: this keyword is used to create a new object
+* 4. Explicit Binding: this is referring to an object/array outside of scope using .call, .appy, or .bind
 *
 * write out a code example of each explanation above
 */
@@ -12,15 +12,62 @@
 // Principle 1
 
 // code example for Window Binding
+function hello(greet) {
+    console.log(this);
+    return greet;
+}
+hello('Hello');
 
 // Principle 2
 
 // code example for Implicit Binding
+const implFunction = function (obj){
+    obj.sayName = function(){
+        console.log(`Name: ${this.name} Age: ${this.age}`);
+    };
+};
+
+const me = {
+    name: 'Brandon',
+    age: 24
+};
+
+const you = {
+    name: 'Mike',
+    age: 44
+}
+
+implFunction(me);
+implFunction(you);
+
+me.sayName();
+you.sayName();
+
 
 // Principle 3
 
 // code example for New Binding
 
+let animal = function(color, type){
+    this.color = color,
+    this.type = type
+    this.speak = function(){
+        console.log(`I am a ${this.color} ${this.type}`)
+    }
+};
+
+const dog = new animal('brown', 'dog');
+
+dog.speak();
+
 // Principle 4
 
 // code example for Explicit Binding
+
+const explFunction = function(arr1, arr2, arr3){
+    console.log(`My name is ${this.name} I am ${this.age} years old and I enjoy ${arr1}, ${arr2}, and ${arr3}.`)
+}
+
+const things = ['coding', 'sleeping', 'thriving'];
+
+explFunction.apply(me, things);
